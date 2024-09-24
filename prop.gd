195e606fa91cddc718f0eec4431feccbd883b4f8
arnold <matrix-yangdq@qq.com>
2024-09-25 00:31:36 +0800

    add rand in prop

--- prop.gd
+++ prop.gd
@@ -1,5 +1,8 @@
 extends Area2D
 signal touch_player
+var operator_list=["+", "-", "x", "÷"]
+var operator='+'
+var num=0
 #var window_size
 #var prop_pos = ""
 # Called when the node enters the scene tree for the first time.
@@ -15,6 +18,10 @@ func _ready() -> void:
 	#else:
 		#position=Vector2(max_x,height*0.1)
 	# Called every frame. 'delta' is the elapsed time since the previous frame.
+	#print(randi() % 25)
+	operator = operator_list[randi() % operator_list.size()]
+	num=randi() % 20+1
+	$Text.text=operator+''+str(num)
 	pass
 	
 func _process(delta: float) -> void:
