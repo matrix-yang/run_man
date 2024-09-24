9094d13fbd592bed43508388453b97a80f353e1a
arnold <matrix-yangdq@qq.com>
2024-09-24 23:14:59 +0800

    fininsh group prop

---
+++ group_prop.gd
@@ -0,0 +1,25 @@
+extends Node2D
+var window_size
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	window_size=get_viewport_rect().size
+	var min_x=0.25*window_size.x
+	#var height = $Prop.size.y
+	position=Vector2(0.5*window_size.x,0)
+	$Prop.position=Vector2(min_x,0)
+	$Prop2.position=Vector2(-min_x,0)
+	
+	# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position.y +=  100 * delta
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
+	pass # Replace with function body.
+
+
+func _on_prop_touch_player() -> void:
+	queue_free()
+	pass # Replace with function body.
--- prop.gd
+++ prop.gd
@@ -1,27 +1,26 @@
 extends Area2D
-var window_size
-var prop_pos = ""
+signal touch_player
+#var window_size
+#var prop_pos = ""
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	window_size=get_viewport_rect().size
-	var min_x=0.25*window_size.x
-	var max_x=0.75*window_size.x
-	var height = $Text.size.y
-	prop_pos = get_meta("prop_pos")
-	
-	if prop_pos=='left': 
-		position=Vector2(min_x,height*0.1)
-	else:
-		position=Vector2(max_x,height*0.1)
+	#window_size=get_viewport_rect().size
+	#var min_x=0.25*window_size.x
+	#var max_x=0.75*window_size.x
+	#var height = $Text.size.y
+	#prop_pos = get_meta("prop_pos")
+	#
+	#if prop_pos=='left': 
+		#position=Vector2(min_x,height*0.1)
+	#else:
+		#position=Vector2(max_x,height*0.1)
 	# Called every frame. 'delta' is the elapsed time since the previous frame.
+	pass
+	
 func _process(delta: float) -> void:
-	position.y +=  100 * delta
-
-
-func _on_visible_on_screen_enabler_2d_screen_exited() -> void:
-	#print('exit screen')
-	queue_free()
-
+	#position.y +=  100 * delta
+	pass
 
 func _on_area_entered(area: Area2D) -> void:
-	queue_free()
+	print(name,' delect _on_area_entered',area.name)
+	touch_player.emit()
