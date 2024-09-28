a5122b0e79e88b70039a811e1d830c253d191888
ydq001815 <matrix-yangdq@qq.com>
2024-09-28 23:48:51 +0800

    shot

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,23 +1,21 @@
-extends Area2D
-@export var speed = 400 # How fast the player will move (pixels/sec).
-var screen_size # Size of the game window.
-@export var direction = 0
-var min_x
-var max_x
+extends  RigidBody2D
+
+var bullet_scene = preload("res://Bullet.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	screen_size = get_viewport_rect().size
-	min_x=0.25*screen_size.x
-	max_x=0.75*screen_size.x
-	var height = $CollisionShape2D.shape.size.y
-	position=Vector2(0.5*screen_size.x,screen_size.y-height*scale.y*0.6)
+	
+	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if Input.is_action_pressed("right"):
-		direction=+1
-	if Input.is_action_pressed("left"):
-		direction=-1
-	position.x += direction * speed * delta
-	position = position.clamp(Vector2(min_x,0), Vector2(max_x,screen_size.y))
+func _on_shoot_timer_timeout() -> void:
+	var bullet_instance = bullet_scene.instantiate()
+	bullet_instance.global_position = global_position
+	#print('player and bullet pos ',bullet_instance.global_position)
+	#bullet_instance.position=Vector2(0,0)
+	#print('--',bullet_instance.position)
+	#add_child(bullet_instance)
+	#print(get_tree().get_current_scene())
+	get_tree().get_current_scene().add_child(bullet_instance)
+	#rint('init 子弹')
+	pass # Replace with function body.
